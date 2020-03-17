2cd79f9011bbfa77b5c460d349c53d6e5f41a8f6
Harã Heique dos Santos <heikacademicos@hotmail.com>
2020-03-17 03:11:12 -0300

    chore: alteração no arquivo dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,24 @@
-# Pip
+# pip
 sudo apt install python3-pip
+pip3 install --upgrade pip
+
+# numpy
+pip3 install numpy
+
+# pandas
+pip3 install pandas
+
+# tensorflow - maybe i will need a setuptools with higher version
+pip3 install tensorflow
+
+# gensim
+pip3 install gensim
+
+# nltk
+pip3 install nltk
+
+echo "\n"
+echo "The packages has been successfully installed:"
+pip3 list
+
 
